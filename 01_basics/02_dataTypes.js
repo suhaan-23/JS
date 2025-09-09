773b31c1ab14495cbfc21f01cf1722937f0d561b
suhaan-23 <suhaanambwani23@gmail.com>
2025-09-09 20:32:30 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+ 
+//alert(3+3) we are using nodejs not browser
+
+console.log(3+3)
+console.log("suhaan")
+
+let age=19
+
+//number
+// bigInt
+// string
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
