3ec070b2b4b976c58dac72aa3806788ae21ba923
suhaan-23 <suhaanambwani23@gmail.com>
2025-09-15 18:12:20 +0000

    coversions

--- 01_basics/03_conversionOpern.js
+++ 01_basics/03_conversionOpern.js
@@ -21,3 +21,13 @@ let someNumber = 33
 let stringNumber= String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+let str1="hello";
+let str2="suhaan";
+let str3= str1+str2
+console.log(str3);
+
+console.log("1"+2);// output-12
+console.log(1 + "2");// output-12
+console.log("1"+2+2);//output-122
+console.log(1+2+"2");
