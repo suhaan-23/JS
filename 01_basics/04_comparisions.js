be821e947b987e1ca64f687028727171cfffe953
suhaan-23 <suhaanambwani23@gmail.com>
2025-09-15 20:55:46 +0000

    comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,12 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+// console.log(2 == 1);
+// console.log(2!=1);
+
+console.log("2">1)//true
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
