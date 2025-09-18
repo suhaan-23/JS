ae7b6243b6e6184a520f0e12e2b9ed4bb164ce2a
suhaan-23 <suhaanambwani23@gmail.com>
2025-09-18 20:22:25 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,22 @@
+const myArr = [0,1,2,3,4,5]
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr);
+// console.log(myArr2);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(0)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(typeof newArr);
