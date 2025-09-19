17739be4fbef597b9653789683c1711b0d502e97
suhaan-23 <suhaanambwani23@gmail.com>
2025-09-19 19:19:29 +0000

    Objects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,38 @@
+// const tinderUser =  new Object()// singleton obj
+const tinderUser={}//non singleton obj
+
+tinderUser.id = "123abc"
+tinderUser.name = "suhaan"
+tinderUser.isloggedIn = "false"
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "abc@gmail.com",
+    fullname: {
+        userfullName: {
+            firstname:"suhaan",
+            lastname:"ambwani"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullName.firstname);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj4 = {5:"a", 6:"b"}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({},obj1, obj2, obj4)
+
+const obj3 ={...obj1,...obj2}
+console.log(obj3);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isloggedIn'));
+
+
