b5306ab8fc1839a7dc6b3ec65ff5dc40c92a94ff
suhaan-23 <suhaanambwani23@gmail.com>
2025-09-19 19:48:13 +0000

    de structure

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,12 +27,25 @@ const obj4 = {5:"a", 6:"b"}
 // const obj3 = Object.assign({},obj1, obj2, obj4)
 
 const obj3 ={...obj1,...obj2}
-console.log(obj3);
+// console.log(obj3);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isloggedIn'));
+// console.log(tinderUser.hasOwnProperty('isloggedIn'));
 
+const course={
+    coursename: "js in hindi",
+    price:"999",
+    courseInstructor:"suhaan"
+}
+const {courseInstructor: instructor} =  course
+console.log(instructor);
+
+// {
+//    "name":"suhaan",
+//     "coursename":"JS",
+//     "price":"free"
+// }
 
